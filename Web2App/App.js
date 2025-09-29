cfb935f8eaf6e7ee3ccceb46bffb6e482ce371e8
floristechnis-git <floristechnisofficial@gmail.com>
2025-09-29 22:59:54 +0530

    Update App.js

--- Web2App/App.js
+++ Web2App/App.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { WebView } from 'react-native-webview';
 import { SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <WebView 
-        source={{ uri: 'https://example.com' }} //REPLACE THE URL WITH YOURS
+        source={{ uri: 'https://example.com' }}
         style={styles.webview}
         allowsFullscreenVideo={true}
         allowsInlineMediaPlayback={true}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
       />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 25,
   },
   webview: {
     flex: 1,
   },
 });
+
+export default App;
